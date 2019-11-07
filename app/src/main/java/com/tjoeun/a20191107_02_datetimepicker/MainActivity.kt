7eb9a002dc5ed35ed3d18b913ba82b663f70f09b
holystar97 <mkrice500@gmail.com>
2019-11-07 22:01:22 +0900

    날짜 시간 정보 저장

--- app/src/main/java/com/tjoeun/a20191107_02_datetimepicker/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191107_02_datetimepicker/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.tjoeun.a20191107_02_datetimepicker
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,10 +15,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun setupEvents(){
 
+        selectDateBtn.setOnClickListener {
+            var datePickerDialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
 
+                dateTxt.text ="${year} 년 ${month+1}월 ${dayOfMonth}일"
+
+                
+            },2019,Calendar.NOVEMBER,9)
+            datePickerDialog.show()
+        }
     }
 
-    override  fun setValues(){
+    override fun setValues(){
 
     }
 
